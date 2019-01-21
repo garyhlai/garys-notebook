b4b5c7befe4db3e74f62cbe688832b6c2b496e81
Gary Lai <garylai@MacBook-Air-de-Gary.local>
2019-01-20 20:06:25 -0500

    configures a workable version of newsletter subscribe

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://xiabi:ruyoushen123@ds161764.mlab.com:61764/garysnote
 app.set("view engine", "ejs");
 app.use(express.static("public"));//for serving custom stylesheet
 app.use(bodyParser.urlencoded({extended: true}));
+//app.use(express.static(__dirname + '/src'));
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -45,9 +46,64 @@ app.get("/blogs/:id", function(req, res){
             res.render("show", {blog: foundBlog});
         }
     })
-})
+});
+
+//Mailchimp Route
+/*
+app.post('/newsletter', function(req, res){
+    addEmailToMailchimp(req.body.subscribe);
+});
+
+function addEmailToMailchimp(subscribe) {
+    var request = require("request");
+    var options = { method: 'POST',
+    url: 'https://us20.api.mailchimp.com/3.0/lists/67883bced5/members',
+    headers: 
+    { 'Postman-Token': '6152c33f-9510-4586-8ab2-f8ce4dcb3ae3',
+        'cache-control': 'no-cache',
+        Authorization: 'Basic YW55c3RyaW5nOmFkYWM4M2QyYzFjMmU3M2RmZTc2MmNlMzhlYTliMDBkLXVzMjA=',
+        'Content-Type': 'application/json' },
+    body: { email_address: subscribe.email, "merge_fields": {
+        "FNAME": subscribe.name}, status: 'subscribed' },
+    json: true };
+    request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+})};
+*/
+
+app.post('/newsletter', function(req, res){
+
+    console.log(req.body);
+    console.log(req.body.email);
+    console.log("here");
+    addEmailToMailchimp(req.body);
+    
+   //res.send(req.body.email); 
+});
+
+function addEmailToMailchimp(reqbody) {
+    var request = require("request");
+    var options = { method: 'POST',
+    url: 'https://us20.api.mailchimp.com/3.0/lists/67883bced5/members',
+    headers: 
+    { 'Postman-Token': '6152c33f-9510-4586-8ab2-f8ce4dcb3ae3',
+        'cache-control': 'no-cache',
+        Authorization: 'Basic YW55c3RyaW5nOmFkYWM4M2QyYzFjMmU3M2RmZTc2MmNlMzhlYTliMDBkLXVzMjA=',
+        'Content-Type': 'application/json' },
+    body: { email_address: reqbody.email, "merge_fields": {
+        "FNAME": reqbody.name}, status: 'subscribed' },
+    json: true };
+    request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+})};
 
 
+
+
+/*
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server is now running..");
 });
+*/
